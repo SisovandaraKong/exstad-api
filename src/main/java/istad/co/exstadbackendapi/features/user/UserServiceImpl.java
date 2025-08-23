8b9d0b68e3a528265c719ae22a41620dbd8b317d
KungSovannda <kungsovannda@gmail.com>
2025-08-23 15:52:01 +0700

    feat: create user

--- src/main/java/istad/co/exstadbackendapi/features/user/UserServiceImpl.java
+++ src/main/java/istad/co/exstadbackendapi/features/user/UserServiceImpl.java
@@ -6,4 +6,32 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final AuthService authService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserResponse createUser(UserRequest userRequest) {
+
+        if(!userRequest.password().equals(userRequest.cfPassword())){
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Passwords do not match");
+        }
+        RegisterRequest register = new RegisterRequest(
+                userRequest.username(),
+                userRequest.email(),
+                userRequest.password(),
+                userRequest.cfPassword(),
+                userRequest.englishName(),
+                userRequest.khmerName()
+        );
+
+        KeycloakUserResponse response = authService.register(register);
+        User user = userMapper.toUser(userRequest);
+        user.setUuid(response.uuid());
+
+        user = userRepository.save(user);
+
+        return userMapper.fromUser(user);
+    }
 }
