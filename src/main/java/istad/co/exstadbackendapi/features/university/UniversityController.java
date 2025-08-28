3ac3a89272b01b43f83727e01ac629e4618255c5
KungSovannda <kungsovannda@gmail.com>
2025-08-28 16:26:40 +0700

    update: uni controller

--- src/main/java/istad/co/exstadbackendapi/features/university/UniversityController.java
+++ src/main/java/istad/co/exstadbackendapi/features/university/UniversityController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/api/v1/universities")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:3000")
 public class UniversityController {
 
     private final UniversityService universityService;
