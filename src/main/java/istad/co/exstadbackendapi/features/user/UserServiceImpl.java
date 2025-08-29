209dfab8f855248a2c051b0d796cb3540e5d3f64
KungSovannda <kungsovannda@gmail.com>
2025-08-29 15:48:08 +0700

    feat: get current user

--- src/main/java/istad/co/exstadbackendapi/features/user/UserServiceImpl.java
+++ src/main/java/istad/co/exstadbackendapi/features/user/UserServiceImpl.java
@@ -92,4 +92,29 @@ public class UserServiceImpl implements UserService {
         return userRepository.findByUuid(uuid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")).getUsername();
 
     }
+
+    @Override
+    public UserResponse getCurrentUser() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof org.springframework.security.oauth2.jwt.Jwt jwt) {
+            username = jwt.getClaimAsString("preferred_username");
+            if (username == null) {
+                username = jwt.getClaimAsString("email");
+            }
+        } else if (principal instanceof UserDetails userDetails) {
+            username = userDetails.getUsername();
+        } else {
+            username = principal.toString();
+        }
+
+
+        return getUserByUsername(username);
+    }
+
+    @Override
+    public String getUsernameByUuid(String uuid) {
+        return userRepository.findByUuid(uuid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")).getUsername();
+
+    }
 }
