ef7b1bf35c0c69c7b4233abb543b1e1888b9cb5c
KungSovannda <kungsovannda@gmail.com>
2025-08-30 16:36:20 +0700

    feat: scholar dto

---
+++ src/main/java/istad/co/exstadbackendapi/features/scholar/dto/ScholarResponse.java
@@ -0,0 +1,41 @@
+package istad.co.exstadbackendapi.features.scholar.dto;
+
+import istad.co.exstadbackendapi.enums.Gender;
+import istad.co.exstadbackendapi.enums.Role;
+
+import java.time.LocalDate;
+
+public record ScholarResponse(
+        String username,
+
+        String email,
+
+        String englishName,
+
+        String khmerName,
+
+        Gender gender,
+
+        LocalDate dob,
+
+        Role role,
+
+        String university,
+
+        String province,
+
+        String currentAddress,
+
+        String nickname,
+
+        String bio,
+
+        String avatar,
+
+        String phoneFamilyNumber,
+
+        Boolean isPublic,
+
+        String quote
+) {
+}
