4ffa06e59d87a893b8e19425c0248be9d29529a8
KungSovannda <kungsovannda@gmail.com>
2025-09-04 04:51:30 +0700

    update: document key for document type

--- src/main/java/istad/co/exstadbackendapi/enums/DocumentType.java
+++ src/main/java/istad/co/exstadbackendapi/enums/DocumentType.java
@@ -8,14 +8,24 @@ import java.util.List;
 @Getter
 @RequiredArgsConstructor
 public enum DocumentType {
-    CERTIFICATE( "certificates", List.of("pdf")),
-    TRANSCRIPT("transcripts", List.of("pdf")),
-    ACTIVITY( "activities", List.of("pdf")),
-    POSTER("images/posters", List.of("jpeg", "jpg", "png", "webp")),
-    THUMBNAIL( "images/thumbnails", List.of("jpeg", "jpg", "png", "webp")),
-    AVATAR( "images/avatars", List.of("jpeg", "jpg", "png", "webp"));
+    CERTIFICATE( "certificate","certificates", List.of("pdf")),
+    TRANSCRIPT("transcript","transcripts", List.of("pdf")),
+    ACTIVITY( "activity","activities", List.of("pdf")),
+    POSTER("poster","images/posters", List.of("jpeg", "jpg", "png", "webp")),
+    THUMBNAIL( "thumbnail","images/thumbnails", List.of("jpeg", "jpg", "png", "webp")),
+    AVATAR( "avatar","images/avatars", List.of("jpeg", "jpg", "png", "webp"));
 
+    private final String key;
     private final String folderPath;
     private final List<String> supportedFiles;
+
+    public static DocumentType fromKey(String key) {
+        for (DocumentType documentType : DocumentType.values()) {
+            if (documentType.key.equals(key.toLowerCase())) {
+                return documentType;
+            }
+        }
+        throw new IllegalArgumentException("Invalid key: " + key);
+    }
 }
 
