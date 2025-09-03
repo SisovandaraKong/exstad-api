36b23d7a583d6a8a1da7b0408a842c924bc903f0
KungSovannda <kungsovannda@gmail.com>
2025-09-04 04:50:15 +0700

    feat: update document and modify upload

--- src/main/java/istad/co/exstadbackendapi/features/document/DocumentController.java
+++ src/main/java/istad/co/exstadbackendapi/features/document/DocumentController.java
@@ -1,11 +1,7 @@
 package istad.co.exstadbackendapi.features.document;
 
-import io.minio.MinioClient;
-import istad.co.exstadbackendapi.enums.DocumentType;
 import istad.co.exstadbackendapi.features.document.dto.DocumentResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.Resource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
