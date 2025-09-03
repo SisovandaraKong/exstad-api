342a208f72e355e47ab1c0fab299895b755c7076
KungSovannda <kungsovannda@gmail.com>
2025-09-04 04:52:00 +0700

    update: update enable audit annotation

--- src/main/java/istad/co/exstadbackendapi/ExstadApi.java
+++ src/main/java/istad/co/exstadbackendapi/ExstadApi.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider", modifyOnCreate = false)
 public class ExstadApi {
 
     public static void main(String[] args) {
