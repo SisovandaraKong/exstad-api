083726b832acd9bfef88038425ef6e09e71da635
KungSovannda <kungsovannda@gmail.com>
2025-09-07 00:55:47 +0700

    feat: define method in achievement repo

--- src/main/java/istad/co/exstadbackendapi/features/achievement/AchievementRepository.java
+++ src/main/java/istad/co/exstadbackendapi/features/achievement/AchievementRepository.java
@@ -1,7 +1,17 @@
 package istad.co.exstadbackendapi.features.achievement;
 
 import istad.co.exstadbackendapi.domain.Achievement;
+import istad.co.exstadbackendapi.domain.OpeningProgram;
+import istad.co.exstadbackendapi.features.achievement.dto.AchievementResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AchievementRepository extends JpaRepository<Achievement, Integer> {
+
+    Optional<Achievement> findByTitle(String title);
+    Optional<Achievement> findByUuid(String uuid);
+
+    List<Achievement> findAllByOpeningProgram(OpeningProgram openingProgram);
 }
