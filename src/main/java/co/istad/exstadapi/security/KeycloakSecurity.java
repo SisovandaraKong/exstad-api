d54aa1143f382bcc31f54f016e513ae4569e2632
Kong_Sisovandara <sisovandara@gmail.com>
2025-09-14 00:11:45 +0700

    scholar profile feature

--- src/main/java/co/istad/exstadapi/security/KeycloakSecurity.java
+++ src/main/java/co/istad/exstadapi/security/KeycloakSecurity.java
@@ -38,11 +38,10 @@ public class KeycloakSecurity {
                 // Register and Login can access by anyone
                 .requestMatchers("/api/auth/**").permitAll()
                 // User and create into our db and keycloak
-                        .requestMatchers("/api/v1/users/**").permitAll()
-//                .requestMatchers("/api/v1/users/register").hasAnyRole("ADMIN","INSTRUCTOR1")
-//                .requestMatchers("/api/v1/users/login").permitAll()
-//                .requestMatchers(HttpMethod.POST,"/api/v1/users/**").hasAnyRole("ADMIN","INSTRUCTOR1")
-//                .requestMatchers(HttpMethod.GET,"/api/v1/users/**").hasAnyRole("ADMIN","INSTRUCTOR1","INSTRUCTOR2")
+                .requestMatchers("/api/v1/users/register").hasAnyRole("ADMIN","INSTRUCTOR1")
+                .requestMatchers("/api/v1/users/login").permitAll()
+                .requestMatchers(HttpMethod.POST,"/api/v1/users/**").hasAnyRole("ADMIN","INSTRUCTOR1")
+                .requestMatchers(HttpMethod.GET,"/api/v1/users/**").hasAnyRole("ADMIN","INSTRUCTOR1","INSTRUCTOR2")
                 // Province
                 .requestMatchers(HttpMethod.GET,"/api/v1/provinces/**").hasAnyRole("ADMIN","INSTRUCTOR1","INSTRUCTOR2")
                 // University
@@ -69,12 +68,11 @@ public class KeycloakSecurity {
                 .requestMatchers(HttpMethod.PATCH,"/api/v1/current-addresses/**").hasAnyRole("ADMIN","INSTRUCTOR1")
                 .requestMatchers(HttpMethod.DELETE,"/api/v1/current-addresses/**").hasAnyRole("ADMIN")
                 // Scholar
-                        .requestMatchers("/api/v1/scholars/**").permitAll()
-//                .requestMatchers(HttpMethod.GET,"/api/v1/scholars/**").hasAnyRole("ADMIN","INSTRUCTOR1","INSTRUCTOR2")
-//                .requestMatchers(HttpMethod.POST,"/api/v1/scholars/**").hasAnyRole("ADMIN","INSTRUCTOR1","INSTRUCTOR2")
-//                .requestMatchers(HttpMethod.PUT,"/api/v1/scholars/**").hasAnyRole("ADMIN","INSTRUCTOR1")
-//                .requestMatchers(HttpMethod.PATCH,"/api/v1/scholars/**").hasAnyRole("ADMIN","INSTRUCTOR1")
-//                .requestMatchers(HttpMethod.DELETE,"/api/v1/scholars/**").hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.GET,"/api/v1/scholars/**").hasAnyRole("ADMIN","INSTRUCTOR1","INSTRUCTOR2")
+                .requestMatchers(HttpMethod.POST,"/api/v1/scholars/**").hasAnyRole("ADMIN","INSTRUCTOR1","INSTRUCTOR2")
+                .requestMatchers(HttpMethod.PUT,"/api/v1/scholars/**").hasAnyRole("ADMIN","INSTRUCTOR1")
+                .requestMatchers(HttpMethod.PATCH,"/api/v1/scholars/**").hasAnyRole("ADMIN","INSTRUCTOR1")
+                .requestMatchers(HttpMethod.DELETE,"/api/v1/scholars/**").hasAnyRole("ADMIN")
                 // Scholar Badge
                 .requestMatchers(HttpMethod.GET,"/api/v1/scholars/**").hasAnyRole("ADMIN","INSTRUCTOR1","INSTRUCTOR2")
                 .requestMatchers(HttpMethod.POST,"/api/v1/scholars/**").hasAnyRole("ADMIN","INSTRUCTOR1","INSTRUCTOR2")
