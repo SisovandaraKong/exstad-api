42138f2a41384ef01703bafc107154d5bd930901
Kong_Sisovandara <sisovandara@gmail.com>
2025-09-18 17:59:55 +0700

    config response json

--- src/main/java/co/istad/exstadapi/domain/vo/LearningOutcome.java
+++ src/main/java/co/istad/exstadapi/domain/vo/LearningOutcome.java
@@ -19,14 +19,9 @@ public class LearningOutcome {
 
     private String uuid = UUID.randomUUID().toString();
 
-    @NotBlank(message = "Title is required")
-    @Size(min = 3, max = 150, message = "Title must be between 3 and 150 characters")
     private String title;
 
-    @NotBlank(message = "Subtitle is required")
-    @Size(min = 3, max = 300, message = "Subtitle must be between 3 and 300 characters")
     private String subtitle;
 
-    @NotEmpty(message = "At least one description is required")
-    private List<@NotBlank(message = "Description must not be blank") String> description;
+    private List<String> description;
 }
