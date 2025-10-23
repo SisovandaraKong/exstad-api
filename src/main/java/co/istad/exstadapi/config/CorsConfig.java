17fef4b7bab1609bd5359ce58f9ba7e20d5bde31
Kong_Sisovandara <sisovandara@gmail.com>
2025-10-23 13:14:53 +0700

    allow all

--- src/main/java/co/istad/exstadapi/config/CorsConfig.java
+++ src/main/java/co/istad/exstadapi/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://www.exstad.tech", "http://localhost:3000", "https://admin.exstad.tech")
+                        .allowedOrigins("*")
                         .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
                         .allowCredentials(true)
                         .allowedHeaders("*");
